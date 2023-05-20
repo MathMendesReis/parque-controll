14f36c22dcb83525ddcf14d036ae8f69c122362a
Matheus Mendes <matheusmendesreis@outlook.com>
2023-05-19 21:36:22 -0300

    first commit

---
+++ src/main/java/com/api/parquecontroll/ParqueControllApplication.java
@@ -0,0 +1,13 @@
+package com.api.parquecontroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueControllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueControllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/parquecontroll/ParqueControllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parquecontroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueControllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
